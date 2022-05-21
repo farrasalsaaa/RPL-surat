15c193169adda53ffb50faa18de9fd7951f2e469
Farras Salsabila <99080670+farrasalsaaa@users.noreply.github.com>
2022-05-21 14:31:39 +0700

    Add files via upload

---
+++ manajemen surat/backup/2022-03-08_203442.sql
@@ -0,0 +1,131 @@
+DROP TABLE tbl_disposisi;
+
+CREATE TABLE `tbl_disposisi` (
+  `id_disposisi` int(10) NOT NULL AUTO_INCREMENT,
+  `tujuan` varchar(250) NOT NULL,
+  `isi_disposisi` mediumtext NOT NULL,
+  `sifat` varchar(100) NOT NULL,
+  `batas_waktu` date NOT NULL,
+  `catatan` varchar(250) NOT NULL,
+  `id_surat` int(10) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_disposisi`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+DROP TABLE tbl_instansi;
+
+CREATE TABLE `tbl_instansi` (
+  `id_instansi` tinyint(1) NOT NULL,
+  `institusi` varchar(150) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `status` varchar(150) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `kepsek` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `website` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `logo` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_instansi`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_instansi VALUES("1","-","PT Angkasa","Terakreditasi A","Jakarta Pusat","Farras ","-","https://angkasa.com","email@angkasa.com","8218.png_860.png","1");
+
+
+
+DROP TABLE tbl_klasifikasi;
+
+CREATE TABLE `tbl_klasifikasi` (
+  `id_klasifikasi` int(5) NOT NULL AUTO_INCREMENT,
+  `kode` varchar(30) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `uraian` mediumtext NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_klasifikasi`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_klasifikasi VALUES("6","A","Pendidikan","Pendidikan Sekolah Menengah Kejuruan","1");
+INSERT INTO tbl_klasifikasi VALUES("7","B","Sarana","Bangunan Sekolah dan Sarana Pendukung Lainnya","1");
+INSERT INTO tbl_klasifikasi VALUES("8","C","Kurikulum","Kurikulum 2016","1");
+INSERT INTO tbl_klasifikasi VALUES("9","D","Kegiatan","Ekstrakurikuler","1");
+INSERT INTO tbl_klasifikasi VALUES("10","E","Administrasi","Administrasi Keuangan kjasfgiasypao mnsagfoaslfas fdhfoa9ydfa fkdfodfiuwoeyfkhsdf sldkhfydoigusldkngsd gsldkhgpsojdfn sd flsaigosdlgfk,bghodiflgdnfk,ngd lkshpdo gjs.nglskdjgskdg","1");
+
+
+
+DROP TABLE tbl_sett;
+
+CREATE TABLE `tbl_sett` (
+  `id_sett` tinyint(1) NOT NULL,
+  `surat_masuk` tinyint(2) NOT NULL,
+  `surat_keluar` tinyint(2) NOT NULL,
+  `referensi` tinyint(2) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_sett`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_sett VALUES("1","10","10","10","1");
+
+
+
+DROP TABLE tbl_surat_keluar;
+
+CREATE TABLE `tbl_surat_keluar` (
+  `id_surat` int(10) NOT NULL AUTO_INCREMENT,
+  `no_agenda` int(10) NOT NULL,
+  `tujuan` varchar(250) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `kode` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_catat` date NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_surat`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+DROP TABLE tbl_surat_masuk;
+
+CREATE TABLE `tbl_surat_masuk` (
+  `id_surat` int(10) NOT NULL AUTO_INCREMENT,
+  `no_agenda` int(10) NOT NULL,
+  `no_surat` varchar(50) NOT NULL,
+  `asal_surat` varchar(250) NOT NULL,
+  `isi` mediumtext NOT NULL,
+  `kode` varchar(30) NOT NULL,
+  `indeks` varchar(30) NOT NULL,
+  `tgl_surat` date NOT NULL,
+  `tgl_diterima` date NOT NULL,
+  `file` varchar(250) NOT NULL,
+  `keterangan` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id_surat`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_surat_masuk VALUES("1","1","32","pt biru","Kerjasama","w01","-","2022-02-25","2022-02-25","6909-T1MSI_4B_Farras20012.pdf","Tidak ada","1");
+
+
+
+DROP TABLE tbl_user;
+
+CREATE TABLE `tbl_user` (
+  `id_user` tinyint(2) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+INSERT INTO tbl_user VALUES("1","admin","ec473d90c37bf91bb29f6b4414baf161","Admin","-","1");
+INSERT INTO tbl_user VALUES("2","user1","24c9e15e52afc47c225b757e7bee1f9d","Tenaga Operasional","001","3");
+
+
+
